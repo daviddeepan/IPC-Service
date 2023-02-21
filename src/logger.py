1cea7c1b1ba4087145eeb42eb6a7746a922833af
daviddeepan <davidideepan@gmail.com>
2023-02-21 20:01:27 +0530

    added docker compose for starting

--- src/logger.py
+++ src/logger.py
@@ -8,7 +8,7 @@ es = Elasticsearch('https://localhost:9200')
 def get_data_from_text_file(self):
     return [l.strip() for l in open(str(self), encoding="utf8", errors='ignore')]
 
-docs = get_data_from_text_file("ipc_id.json")
+docs = get_data_from_text_file("ipc_id.json") 
 print ("String docs length:", len(docs))
 
 doc_list =[]
@@ -22,20 +22,20 @@ for num, doc in enumerate(docs):
         print ("ERROR for num:", num, "-- JSONDecodeError:", err, "for doc:", doc)
 
 print ("Dict docs length:", len(doc_list))
+print(doc_list)
 
+# try:
+#     print ("\nAttempting to index the list of docs using helpers.bulk()")
+#     resp = helpers.bulk(
+#         es,
+#         doc_list,
+#         index = "full_index",
+#         doc_type = "_doc"
+#     )
 
-try:
-    print ("\nAttempting to index the list of docs using helpers.bulk()")
-    resp = helpers.bulk(
-        es,
-        doc_list,
-        index = "full_index",
-        doc_type = "_doc"
-    )
+#     print ("helpers.bulk() RESPONSE:", resp)
+#     print ("helpers.bulk() RESPONSE:", json.dumps(resp, indent=4))
 
-    print ("helpers.bulk() RESPONSE:", resp)
-    print ("helpers.bulk() RESPONSE:", json.dumps(resp, indent=4))
-
-except Exception as err:
-    print("Elasticsearch helpers.bulk() ERROR:", err)
-    quit()    
+# except Exception as err:
+#     print("Elasticsearch helpers.bulk() ERROR:", err)
+#     quit()    
